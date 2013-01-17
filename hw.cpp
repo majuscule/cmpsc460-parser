8c703f7c4cb684ae14547df7e32ce780fa6b2e46
Dylan Lloyd <dylan@dylansserver.com>
2013-01-17 04:31:43 -0500

    simplify

--- hw.cpp
+++ hw.cpp
@@ -9,12 +9,10 @@ using namespace std;
 
 const string prompt = "> ";
 
-enum token {
-  SEPARATOR,
-  SPACE,
-  NUMBER,
-  OPERATOR,
-  CONDITIONAL
+struct expression {
+  int a;
+  int b;
+  string op;
 };
 
 void error(int charNum, string msg) {
@@ -29,6 +27,7 @@ string read() {
 }
 
 int* parse(string command, int operands[]){
+  expression exp;
   int operand = 0;
   if (command[0] != '(') {
     error(0, "expected SEPARATOR");
@@ -37,65 +36,17 @@ int* parse(string command, int operands[]){
     error(command.length(), "expected trailing ')' to end expression");
     return 0;
   }
+  command = command.substr(1, command.length()-2);
   istringstream split(command); 
-  string each = "";
-  while (getline(split, each, ' ')) {
-          puts(each.c_str());
-  }
-  token last_seen = SEPARATOR;
-  int i = 1;
-  while (i < command.length()) {
-    if (last_seen == SEPARATOR) {
-      if (isdigit(command[i])) {
-        last_seen = NUMBER;
-        operands[operand] += static_cast<int>(command[i] - '0');
+  string word = "";
+  while (getline(split, word, ' ')) {
+      if (stringstream(word) >> operands[operand]) {
         operand++;
-      } else if (command[i] == 'I') {
-          if ( i+1 < command.length()
-                  && command[i+1] == 'F') {
-            last_seen = CONDITIONAL;
-            i++;
-          } else {
-            error(i, "expected 'IF' conditional");
-            return 0;
-          }
       } else {
-        error(i, "expected a DIGIT or an IF statement");
-        return 0;
+        exp.op = word;
       }
-    } else if (last_seen == NUMBER) {
-        if (command[i] == ' ') {
-            last_seen = SPACE;
-        } else if (!isdigit(command[i])) {
-            error(i, "expected a SPACE or a DIGIT");
-            return 0;
-        }
-    } else if (last_seen == OPERATOR) {
-      if (command[i] != ')') {
-        error(i, "expected a ')' character to end the expression");
-        return 0;
-      }
-    } else if (last_seen == SPACE) {
-      if (! (isdigit(command[i])
-                || command[i] == ')'
-                || command[i] == '+')) {
-          fprintf(stderr, "  warn: =%c=\n", command[i]);
-          error(i, "expected DIGIT, SEPARATOR, or OPERATOR");
-          return 0;
-      } else if (isdigit(command[i])) {
-          operands[operand] += static_cast<int>(command[i] - '0');
-          operand++;
-      }
-
-      if (isdigit(command[i])) {
-        last_seen = NUMBER;
-      }
-    } else {
-      error(i, "expected SEPARATOR, DIGIT, or IF statement");
-      return 0;
-    }
-    i++;
   }
+
   return operands;
 }
 
